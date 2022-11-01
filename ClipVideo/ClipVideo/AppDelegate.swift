a6eb27e87346e3a70cdd6ac0da0e938b375d8a9a
una_sheng <1150363674@qq.com>
2022-11-01 14:15:54 +0800

    Add files via upload

---
+++ ClipVideo/ClipVideo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ClipVideo
+//
+//  Created by li.wenxiu on 2022/11/1.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ ClipVideo/ClipVideo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ClipVideo
+//
+//  Created by li.wenxiu on 2022/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
